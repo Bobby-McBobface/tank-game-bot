0421e1b919439f791006e07a99cc13bca77d0f21
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2023-06-21 20:07:57 +0800

    feat: board command

---
+++ src/commands/board.ts
@@ -0,0 +1,77 @@
+import type { PlayersRecord } from '#lib/database';
+import { Command, RegisterCommand } from '@skyra/http-framework';
+import { createCanvas, loadImage } from 'canvas';
+
+@RegisterCommand((builder) =>
+	builder //
+		.setName('board')
+		.setDescription('View the board.')
+)
+export class UserCommand extends Command {
+	private canvas = createCanvas(320, 200);
+	private ctx = this.canvas.getContext('2d');
+
+	public override async chatInputRun(interaction: Command.ChatInputInteraction) {
+		await interaction.defer();
+
+		const players = await this.container.pocketbase
+			.collection('players')
+			.getFullList<PlayersRecord>({ filter: `guild_id='${interaction.guild_id}'` });
+
+		await this.createGrid(players);
+
+		await interaction.followup({
+			attachments: [{ id: '0', filename: 'board.png' }],
+			files: [{ name: 'board.png', data: this.canvas.toBuffer() }]
+		});
+	}
+
+	private drawCell(x: number, y: number, color: string) {
+		// Set the fill style to the given color
+		this.ctx.fillStyle = color;
+		// Draw a rectangle with 20 by 20 pixels at the given x and y coordinates
+		this.ctx.fillRect(x * 20, y * 20, 20, 20);
+		// Set the stroke style to black
+		this.ctx.strokeStyle = 'black';
+		// Draw a border around the rectangle
+		this.ctx.strokeRect(x * 20, y * 20, 20, 20);
+	}
+
+	// Define a function to draw an avatar
+	private async drawAvatar(x: number, y: number, player: PlayersRecord) {
+		// Get the file URL from the pocketbase instance using the avatar string
+		const fileURL = this.container.pocketbase.getFileUrl(player, player.avatar);
+		// Set the image source to the file URL
+		const image = await loadImage(Buffer.from(await (await fetch(fileURL)).arrayBuffer()));
+		// Draw the image with 20 by 20 pixels at the given x and y coordinates
+		this.ctx.drawImage(image, x * 20, y * 20, 20, 20);
+	}
+
+	// Define a function to create the grid image
+	private async createGrid(players: PlayersRecord[]) {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		// Loop through the rows and columns of the grid
+		for (let x = 0; x < 20; x++) {
+			for (let y = 0; y < 20; y++) {
+				// Define the color variable
+				let color: string;
+				// If the sum of x and y is even, set the color to dark green
+				if ((x + y) % 2 === 0) {
+					color = '#006400';
+				} else {
+					// Otherwise, set the color to very dark green
+					color = '#003300';
+				}
+				// Draw an empty cell with the chosen color
+				this.drawCell(x, y, color);
+				// Query the database for a user at the current position
+				const row = players.find((v) => v.x_pos === x && v.y_pos === y);
+
+				// If there is a user, draw their avatar
+				if (row) {
+					await this.drawAvatar(x, y, row);
+				}
+			}
+		}
+	}
+}
